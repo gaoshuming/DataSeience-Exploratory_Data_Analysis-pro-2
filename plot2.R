2b3227818e85d290f2c406a2cc0b79d748616e3a
gaoshuming <gaoshuming121@163.com>
2014-10-16 17:07:27 +0800

    Delete plot2.R

--- plot2.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-png(filename = "plot2.png", width = 480, height = 480)
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
-qplot(year,Emissions,data = NEI[which(NEI$fips == 24510),])
-dev.off()
